13457d255e785642ba5d11e4e499433f43ed9a89
itmanharry <jack_chengqian@163.com>
2021-08-27 16:22:30 +0800

    department edited finished

--- pm/forms/sys/org.py
+++ pm/forms/sys/org.py
@@ -19,14 +19,39 @@ class OrgForm(FlaskForm):
     print('Dept list is >>>>>>>>>> ', department_list)
     '''
     #department_list = []
+    id = HiddenField()
     code = StringField('部门代码', validators=[DataRequired('请输入部门代码！')])
     name = StringField('部门名称', validators=[DataRequired('请输入部门名称！')])
     parent = SelectField('上级部门', [validators.optional()], choices=[])
     has_parent = HiddenField()      # 是否有上级部门(1:有 0：没有， 默认为有)
 
     def validate_code(self, field):
-        if BizDept.query.filter_by(code=field.data.lower()).first():
-            raise ValidationError('部门代码已存在!')
+        if self.id.data == '':
+            if BizDept.query.filter_by(code=field.data.lower()).first():
+                raise ValidationError('部门代码已存在!')
+        else:
+            old_code = BizDept.query.get(self.id.data).code
+            codes = []
+            all_departments = BizDept.query.all()
+            for department in all_departments:
+                codes.append(department.code)
+            # 剔除未更新前的部门代码
+            codes.remove(old_code)
+            # Check新的部门代码是否已经存在
+            if field.data.lower() in codes:
+                raise ValidationError('部门代码已存在!')
     def validate_name(self, field):
-        if BizDept.query.filter_by(name=field.data).first():
-            raise ValidationError('部门名称已存在!')
+        if self.id.data == '':
+            if BizDept.query.filter_by(name=field.data).first():
+                raise ValidationError('部门名称已存在!')
+        else:
+            old_name = BizDept.query.get(self.id.data).name
+            names = []
+            all_departments = BizDept.query.all()
+            for department in all_departments:
+                names.append(department.name)
+            # 剔除未更新前的部门名称
+            names.remove(old_name)
+            # Check新的部门名称是否已经存在
+            if field.data in names:
+                raise ValidationError('部门名称已存在!')
--- pm/views/sys/org.py
+++ pm/views/sys/org.py
@@ -20,8 +20,8 @@ def index():
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['ITEM_COUNT_PER_PAGE']
     pagination = BizDept.query.filter(BizDept.code.like('%'+code+'%'), BizDept.name.like('%'+name+'%')).order_by(BizDept.code).paginate(page, per_page)
-    orgs = pagination.items
-    return render_template('sys/org/index.html', pagination=pagination, orgs=orgs, form=form)
+    departments = pagination.items
+    return render_template('sys/org/index.html', pagination=pagination, departments=departments, form=form)
 
 @bp_org.route('/add', methods=['GET', 'POST'])
 @login_required
@@ -39,6 +39,42 @@ def add():
         has_parent = form.has_parent.data
         if has_parent and form.parent.data is not None:
             department.set_parent_dept(BizDept.query.get(form.parent.data))
-        flash('部门添加成功！')
+        flash('部门信息添加成功！')
         return redirect(url_for('.add'))
     return render_template('sys/org/add.html', form=form)
+@bp_org.route('/eidt/<id>', methods=['GET', 'POST'])
+def edit(id):
+    form = OrgForm()
+    edit_department = BizDept.query.get_or_404(id)
+    '''
+        设置上级部门下拉列表
+        注:上级部门下拉列表需剔除当前部门及子部门
+    '''
+    self_and_children = [edit_department.id]
+    child_departments = edit_department.my_child_dept
+    if child_departments:
+        for child in child_departments:
+            self_and_children.append(child.child_dept_id)
+    departments = BizDept.query.order_by(BizDept.code).all()
+    department_list = []
+    for department in departments:
+        if department.id not in self_and_children:
+            department_list.append((department.id, department.name))
+    form.parent.choices = department_list
+    if request.method == 'GET':
+        form.id.data = edit_department.id
+        form.code.data = edit_department.code
+        form.name.data = edit_department.name
+        form.parent.data = edit_department.my_parent_dept.id if edit_department.my_parent_dept else ''
+    if form.validate_on_submit():
+        edit_department.code = form.code.data
+        edit_department.name = form.name.data
+        db.session.commit()
+        has_parent = form.has_parent.data
+        if has_parent and form.parent.data is not None:
+            edit_department.set_parent_dept(BizDept.query.get(form.parent.data))
+        else:
+            print('不执行上级部门更新')
+        flash('部门信息更新成功！')
+        return redirect(url_for('.edit', id=form.id.data))
+    return render_template('sys/org/edit.html', form=form)
