5466b41b0f38b96c42c9df725377dd1159d33aca
itmanharry <jack_chengqian@163.com>
2021-09-08 11:17:14 +0800

    add program module

---
+++ pm/forms/biz/program.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, HiddenField
+from wtforms.validators import DataRequired
+from wtforms import ValidationError, validators
+from pm.models import BizProgram
+class ProgramSearchForm(FlaskForm):
+    no = StringField('项目编号', [validators.optional()])
+    name = StringField('项目名称', [validators.optional()])
+class ProgramForm(FlaskForm):
+    id = HiddenField()
+    no = StringField('项目编号', [validators.optional()]) # 项目编号自动生成
+    name = StringField('项目名称', validators=[DataRequired('请输入项目名称！')])
+    pr = StringField('PR编号', [validators.optional()])
+    contract = StringField('合同编号', [validators.optional()])
+    svn = StringField('SVN地址', [validators.optional()])
+    desc = TextAreaField('项目描述', [validators.optional()])
+
+    def validate_name(self, field):
+        if self.id.data == '':
+            if BizProgram.query.filter_by(name=field.data).first():
+                raise ValidationError('项目名称已存在!')
+        else:
+            old_name = BizProgram.query.get(self.id.data).name
+            names = []
+            for program in BizProgram.query.all():
+                names.append(program.name)
+            # 剔除未更新前的部门名称
+            names.remove(old_name)
+            # Check新的部门名称是否已经存在
+            if field.data in names:
+                raise ValidationError('项目名称已存在!')
--- pm/views/biz/program.py
+++ pm/views/biz/program.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, render_template, session
-from flask_login import login_required
+from flask import Blueprint, render_template, session, url_for, request, current_app
+from flask_login import login_required, current_user
+from pm.forms.biz.program import ProgramForm, ProgramSearchForm
+from pm.models import BizProgram
+from pm.plugins import db
+from pm.decorators import log_record
 bp_pro = Blueprint('pro', __name__)
 @bp_pro.route('/index')
 @login_required
+@log_record('查看项目清单')
 def index():
-    session['nav'] = 'pro'
-    return 'OK'
-    #return render_template('sys/index.html')
+    no = ''     # 项目编号
+    name = ''   # 项目名称
+    form = ProgramSearchForm()
+    if request.method == 'POST':
+        no = form.no.data
+        name = form.name.data
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['ITEM_COUNT_PER_PAGE']
+    pagination = BizProgram.query.with_parent(current_user).filter(BizProgram.no.like('%' + no + '%'), BizProgram.name.like('%' + name + '%')).order_by(BizProgram.timestamp_loc).paginate(page, per_page)
+    programs = pagination.items
+    return render_template('biz/program/index.html', form=form, programs=programs, pagination=pagination)
